187b33ea501ba1b59daf5fb589da678eaa0d4c48
Naxsor <lemanuel.colon@upr.edu>
2021-02-22 19:57:30 -0400

    -stantalon sttings

--- adapts/settings.py
+++ adapts/settings.py
@@ -24,12 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = [
-#    'adapts.herokuapp.com'
-    '192.168.0.10'
-]
+ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
@@ -155,8 +152,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = lemanuel.colon@upr.edu
+EMAIL_HOST_PASSWORD = rgxhcrwdfswptjsf
 
 #AWS_S3_REGION_NAME = 'us-east-1'
 #AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
