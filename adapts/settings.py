2c15c3ba3787946ae4cfad9007a2d199b1fb0e9b
naxsor <lemanuel.colon@upe.edu>
2021-02-22 20:08:09 -0400

    -settings set for stand-alone run.

--- adapts/settings.py
+++ adapts/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '5yw(wp7(@meq4=(c3_3@2qtmmoyxa2k&0=u3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -152,8 +152,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = lemanuel.colon@upr.edu
-EMAIL_HOST_PASSWORD = rgxhcrwdfswptjsf
+EMAIL_HOST_USER = 'lemanuel.colon@upr.edu'
+EMAIL_HOST_PASSWORD = 'rgxhcrwdfswptjsf'
 
 #AWS_S3_REGION_NAME = 'us-east-1'
 #AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
