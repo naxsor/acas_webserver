26b8d09f509f8fa79b76247ddd2b531ebbc99792
Lemanuel <lemanuel.colon@upr.edu>
2021-07-06 10:44:23 -0400

    -Added download denied page -Fixed base names -Fixed Space -Added decorator to download denied view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 def app_about(request):
     return render(request, 'app_about.html', {'title': 'ADAPTS About'})
 
+@login_required
 def app_download(request):
-    return render(request, 'app_download.html', {'title': 'ADAPTS Download'})
+    if(request.user.profile.project.active == True):
+        return render(request, 'app_download.html', {'title': 'ADAPTS Download'})
+    else:
+        return render(request, 'app_download_denied.html', {'title': 'ADAPTS Download'})
